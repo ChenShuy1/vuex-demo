8ec21ce2c0e61e637407b311ef0d2fe5cf3d6d17
unknown <chenshuyi1@cn.net.ntes>
2018-07-13 17:31:23 +0800

    完成基本的组件

---
+++ src/components/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>TODOS</h1>
+    <input class="new-todo"
+      autofocus
+      autocomplete="off"
+      placeholder="What needs to be done?"
+      @keyup.enter="addTodo">
+    <ul class="todo-list">
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem
+  },
+  data () {
+    return {
+      todos: [{
+        content: '112',
+        completed: true
+      }, {
+        content: '1232',
+        completed: false
+      }],
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
